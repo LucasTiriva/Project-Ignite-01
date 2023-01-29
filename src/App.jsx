d4dfcfb9059fdfe1afcb49893e67a42113d81966
LucasTiriva <lucastiriva@gmail.com>
2023-01-28 21:23:31 -0300

    construindo a barra lateral e uns bugs junto :(

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import { Header } from "./components/Header";
 import { Post } from "./post";
 
-import './global.css';
+import styles from "./App.module.css";
+
+import "./global.css";
 
 export function App() {
   return (
     <div>
-
       <Header />
-      
-      <Post
-        author="Lucas Viana"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, modi!"
-      />
-      <Post 
-        author="Wilha boca berta"
-        content="Moscando de num compra um mine pra mim"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Lucas Viana"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, modi!"
+          />
+          <Post
+            author="Wilha boca berta"
+            content="Moscando de num compra um mine pra mim"
+          />
+        </main>
+      </div>
     </div>
   );
 }
